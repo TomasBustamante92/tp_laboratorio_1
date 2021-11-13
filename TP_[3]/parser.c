ac07ca7f948f59bf22ee0e5b4416080c7b3b295d
TomasBustamante92 <tom.sb92@gmail.com>
2021-11-13 07:14:33 -0600

    data.csv limpio

--- TP_[3]/parser.c
+++ TP_[3]/parser.c
@@ -70,6 +70,12 @@ int parser_EmployeeFromBinary(FILE* pFile , LinkedList* pArrayListEmployee)
     return retorno;
 }
 
+/// @fn int parser_EmployeeToText(FILE*, LinkedList*)
+/// @brief Parsea los datos de los empleados hacia el archivo data.csv (modo texto)
+///
+/// @param pFile
+/// @param pArrayListEmployee
+/// @return si sale bien devuelve [1] caso contrario devuelve [0]
 int parser_EmployeeToText(FILE* pFile , LinkedList* pArrayListEmployee)
 {
 	int retorno = 0;
@@ -93,7 +99,12 @@ int parser_EmployeeToText(FILE* pFile , LinkedList* pArrayListEmployee)
     return retorno;
 }
 
-
+/// @fn int parser_EmployeeToBinary(FILE*, LinkedList*)
+/// @brief Parsea los datos de los empleados hacia el archivo data.bin (modo binario)
+///
+/// @param pFile
+/// @param pArrayListEmployee
+/// @return si sale bien devuelve [1] caso contrario devuelve [0]
 int parser_EmployeeToBinary(FILE* pFile , LinkedList* pArrayListEmployee)
 {
 	int retorno = 0;
--- TP_[3]/parser.h
+++ TP_[3]/parser.h
@@ -6,19 +6,5 @@
 
 int parser_EmployeeFromText(FILE* pFile , LinkedList* pArrayListEmployee);
 int parser_EmployeeFromBinary(FILE* pFile , LinkedList* pArrayListEmployee);
-
-/// @fn int parser_EmployeeToText(FILE*, LinkedList*)
-/// @brief Parsea los datos de los empleados hacia el archivo data.csv (modo texto)
-///
-/// @param pFile
-/// @param pArrayListEmployee
-/// @return si sale bien devuelve [1] caso contrario devuelve [0]
 int parser_EmployeeToText(FILE* pFile , LinkedList* pArrayListEmployee);
-
-/// @fn int parser_EmployeeToBinary(FILE*, LinkedList*)
-/// @brief Parsea los datos de los empleados hacia el archivo data.bin (modo binario)
-///
-/// @param pFile
-/// @param pArrayListEmployee
-/// @return si sale bien devuelve [1] caso contrario devuelve [0]
 int parser_EmployeeToBinary(FILE* pFile , LinkedList* pArrayListEmployee);
